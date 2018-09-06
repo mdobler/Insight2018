403d02eb4f1fc0a858062e2be6233030eb44c3bf
Michael Dobler <m.dobler@steepvalley.net>
2018-09-06 17:46:05 -0600

    Add files via upload
    Initial Commit

---
+++ Introduction to the new Web API for DPS/2018InsightSample.WebApp/Models/VantagePointSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsightSample.WebApp.Models
+{
+    public class VantagePointSettings
+    {
+        public string BaseURL { get; set; } = "";
+        public string ClientId { get; set; } = "";
+        public string ClientSecret { get; set; } = "";
+        public string Database { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
---
+++ Introduction to the new Web API for DPS/2018InsightSample.WebApp/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using InsightSample.WebApp.Models;
+
+namespace InsightSample.WebApp.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpClient> GetAuthorizedVantagePointClientAsync();
+        HttpClient GetVantagePointClient();
+        Task<TokenInfo> GetVantagePointTokenAsync(HttpClient client);
+        Task<dynamic> GetAsync(HttpClient authorizedClient, string requestUri);
+        Task<dynamic> PostAsync(HttpClient authorizedClient, string requestUri, dynamic data);
+        Task<T> GetAsync<T>(HttpClient authorizedClient, string requestUri);
+        Task<T> PostAsync<T>(HttpClient authorizedClient, string requestUri, dynamic data);
+    }
+}
